dc47023910ba7fdcff277e625f3394526999a456
MellowZippy1 <basicconsideration1101@gmail.com>
2024-05-13 21:34:54 +0200

    Button does something

--- src/Voorraad.WebApp/Pages/SuccessPage.cshtml.cs
+++ src/Voorraad.WebApp/Pages/SuccessPage.cshtml.cs
@@ -11,5 +11,12 @@ namespace Voorraad.WebApp.Pages
         {
             // This method is executed when the page is requested via HTTP GET
         }
+
+        public void OnPostBtnLogin_Click1(int index)
+        {
+            Console.WriteLine($"HI! {index} yaya");
+            // Add your server-side logic here
+        }
+
     }
 }
