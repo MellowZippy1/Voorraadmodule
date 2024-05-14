1164fd9b7900ebffd9155e797a2c7ed0c0fea83f
Charriexx <charlotte.e.kwok@gmail.com>
2024-05-14 12:23:04 +0200

    delete button does something

--- src/Voorraad.WebApp/Pages/SuccessPage.cshtml.cs
+++ src/Voorraad.WebApp/Pages/SuccessPage.cshtml.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
+using Npgsql;
 
 namespace Voorraad.WebApp.Pages
 {
     public class SuccessPageModel : PageModel
     {
+        private readonly ILogger<SuccessPageModel> _logger;
+
+        public SuccessPageModel(ILogger<SuccessPageModel> logger)
+        {
+            _logger = logger;
+        }
         public List<string[]> MonteurResults { get; set; }
 
         public void OnGet()
@@ -14,8 +21,28 @@ namespace Voorraad.WebApp.Pages
 
         public void OnPostBtnLogin_Click1(int index)
         {
-            Console.WriteLine($"HI! {index} yaya");
             // Add your server-side logic here
+            try
+            {
+                var connString = "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=postgres";
+
+                using var conn = new NpgsqlConnection(connString);
+                conn.OpenAsync();
+
+                using (var cmd = new NpgsqlCommand("DELETE FROM packages WHERE packageid = @packageId", conn))
+                {
+                    cmd.Parameters.AddWithValue("@packageID", index);
+                    cmd.ExecuteNonQueryAsync();
+                }
+
+                RedirectToPage("/SuccesPage");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing the remove operation.");
+                ModelState.AddModelError(string.Empty, "An error occurred while processing your request.");
+                Page();
+            }
         }
 
     }
